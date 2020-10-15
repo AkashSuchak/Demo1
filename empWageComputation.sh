cae7966a9a99e26cddf751081db9e21820ccd680
AkashSuchak <suchak.akash@gmail.com>
2020-10-15 18:28:42 +0530

    Check Employee is Present or Absent

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
 
 #Author : Akash Suchak
-#Display Welcome Message
+#Employee Attendence
 
-#Welcome Message
-echo "Welcome to Employee Wage Computation Program On master Branch"
+#Varible to take value 0 or 1
+isPresent=$((RANDOM%2))
+
+#Checking Emplyoyee Present or Absent
+if [ "$isPresent" -eq "1" ];then
+	echo "Employee is Present"
+else
+	echo "Employee is Absent"
+fi
 
