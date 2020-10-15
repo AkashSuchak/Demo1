2f8c066f852e361def4113ae4f2d2cae77347ed0
AkashSuchak <suchak.akash@gmail.com>
2020-10-15 22:52:02 +0530

    Monthly Wage and Daily Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #Author : Akash Suchak
-#Daily Employee Wage
+#Monthly and Daily Wage of PartTime and FullTIme Employee
 
 #Varible to take value
 isFullTime=$((RANDOM%3))
@@ -14,15 +14,22 @@ dayHoursPartTime=4
 dailyWageFullTime=$((wagePerHour*dayHoursFullTime))
 dailyWagePartTime=$((wagePerHour*dayHoursPartTime))
 
+#count of Monthly Wage
+workingDays=20
+monthlyWageFullTime=$((workingDays*dailyWageFullTime))
+monthlyWagePartTime=$((workingDays*dailyWagePartTime))
+
 #Checking Part Time or Full Time Emplyoyee with Wage Using Case
 case $isFullTime in
 	1)
 		echo "Full Time Employee"
 		echo "Daily Wage : " $dailyWageFullTime
+		echo "Monthly Wage : " $monthlyWageFullTime
 		;;
 	2)
 		echo "Part Time Employee"
 		echo "Daily Wage : " $dailyWagePartTime
+		echo "Monthly Wage : " $monthlyWagePartTime
 		;;
 	*)
 		echo "Employee is Absent"
