d4bf97e327afded2e2e0f2b2e335ca898d90d199
AkashSuchak <suchak.akash@gmail.com>
2020-10-15 21:10:48 +0530

    Daily Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,14 +1,20 @@
 #! /bin/bash
 
 #Author : Akash Suchak
-#Employee Attendence
+#Daily Employee Wage
 
 #Varible to take value 0 or 1
 isPresent=$((RANDOM%2))
 
+#Count of Daily Wage
+wagePerHour=20
+dayHours=8
+dailyWage=$((wagePerHour*dayHours))
+
 #Checking Emplyoyee Present or Absent
 if [ "$isPresent" -eq "1" ];then
 	echo "Employee is Present"
+	echo "Daily Employee Wage is : " $dailyWage
 else
 	echo "Employee is Absent"
 fi
